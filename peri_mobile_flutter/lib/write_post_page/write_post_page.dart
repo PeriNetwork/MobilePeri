28426d5c8fba5374221ac08f1411124e868220ad
vxpresto <nallinguilherme@gmail.com>
2023-05-10 21:32:34 -0300

    create post final

--- peri_mobile_flutter/lib/write_post_page/write_post_page.dart
+++ peri_mobile_flutter/lib/write_post_page/write_post_page.dart
@@ -12,37 +12,84 @@ class WritePostPage extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.black,
         appBar: PreferredSize(
-            child: PeriNavBar(), preferredSize: const Size.fromHeight(50)),
-        body: Column(
-          
+            preferredSize: const Size.fromHeight(50), child: PeriNavBar()),
+        body: Stack(
           children: [
-            Container(
-              child: Row(
-                children: [
-                  Column(
-                    children: [
-                      Image.asset(
-                        'assets/images/user_peri_nav.png',
-                        height: 50,
-                      )
-                    ],
-                  ),
-                  Container(
-                    width: 0.8 * (MediaQuery.of(context).size.width),
-                    decoration: BoxDecoration(color: Colors.grey[900], borderRadius: BorderRadius.circular(10)),
-                    child: Expanded(
-                        child: TextField(
-                          minLines: 1,
-                          maxLines: 10,
-                      decoration: InputDecoration(
-                        
-                        border: OutlineInputBorder()
-                      ), style: TextStyle(color: Colors.white),
-                    )),
-                  )
-                ],
-              ),
-            )
+            Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/user_peri_nav.png',
+                          height: 50,
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          width: 0.8 * (MediaQuery.of(context).size.width),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[900],
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Expanded(
+                              child: TextField(
+                            autofocus: true,
+                            minLines: 1,
+                            maxLines: 10,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          )),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+            Positioned(
+                top: 0.7 * (MediaQuery.of(context).size.height),
+                left: 0.75 * (MediaQuery.of(context).size.width),
+                child: Column(
+                  children: [
+                    FloatingActionButton(
+                        backgroundColor: Colors.red[400],
+                        onPressed: () {},
+                        child: Icon(
+                          Icons.add_a_photo,
+                          color: Colors.white,
+                        )),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                      width: 75,
+                      height: 75,
+                      child: FloatingActionButton(
+                          backgroundColor: Colors.red,
+                          onPressed: () {},
+                          child: Icon(
+                            Icons.create,
+                            color: Colors.white,
+                          )),
+                    )
+                  ],
+                ))
           ],
         ));
   }

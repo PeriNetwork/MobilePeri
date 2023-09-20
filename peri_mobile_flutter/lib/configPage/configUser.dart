c9da8b84c4a29247fa6ff7fa018105f71735a43e
Vinicius <viniciuscsf1808@gmail.com>
2023-09-20 11:26:14 -0300

    config

--- peri_mobile_flutter/lib/configPage/configUser.dart
+++ peri_mobile_flutter/lib/configPage/configUser.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class configUserPage extends StatefulWidget {
   const configUserPage({super.key});
@@ -10,6 +11,40 @@ class configUserPage extends StatefulWidget {
 class _configUserPageState extends State<configUserPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sua App"),
+        leading: Icon(Icons.menu),
+        actions: [
+          Icon(FontAwesomeIcons.bell),
+          SizedBox(width: 16),
+          CircleAvatar(
+            backgroundImage: NetworkImage("URL_DA_FOTO"),
+          ),
+          SizedBox(width: 16),
+        ],
+      ),
+      body: UserSettingsPage(),
+    );
   }
 }
+
+class UserSettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red, // Cor da AppBar
+      child: Center(
+        child: Text(
+          "Página de Configurações de Usuário",
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+    
